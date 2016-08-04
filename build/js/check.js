e8299f67eca799b03d3cc06d519e57cdbb6aa2ff
Dmitry Bykin <dmitrybykin@gmail.com>
2016-08-04 17:34:24 +0400

    Исправления задания Основы программирования

--- build/js/check.js
+++ build/js/check.js
@@ -1,22 +1,29 @@
 var getMessage = function(a, b){
 
-	if (typeof a == "boolean") {
-		if(a) return "Переданное GIF-изображение анимировано и содержит [" + b + "] кадров";
-		else return "Переданное GIF-изображение не анимировано";
+	if (typeof(a) === "boolean") {
+		if(a) {
+			return "Переданное GIF-изображение анимировано и содержит [" + b + "] кадров";
+		}
+		return "Переданное GIF-изображение не анимировано";
 	}
-	if (typeof a == "number") {
+	if (typeof(a) === "number") {
 		return "Переданное SVG-изображение содержит [" + a + "] объектов и [" + b * 4 + "] атрибутов";
 	}
-	if (Array.isArray(a) && !Array.isArray(b)) {
-		var amountOfRedPoints = 0;
-		for(var i=0;i<a.length;i++) amountOfRedPoints += a[i];
+	if (a) {
+		if(b) {
+			var minLength = Math.min(a.length, b.length);
+			var artifactsSquare = 0;
+			var i;
+			for(i = 0; i < minLength; i++) {
+				artifactsSquare += a[i]*b[i];
+			}
+			return "Общая площадь артефактов сжатия: [" + artifactsSquare + "] пикселей";
+		}
+		var amountOfRedPoints = a.reduce(function(sum, current){
+			return sum + current;
+		}, 0);
 		return "Количество красных точек во всех строчках изображения: [" + amountOfRedPoints + "]";
 	}
-	if(Array.isArray(a) && Array.isArray(b)){
-		var artifactsSquare = 0;
-		for(var i=0;i<a.length;i++) artifactsSquare+= a[i]*b[i];
-		return "Общая площадь артефактов сжатия: [" + artifactsSquare + "] пикселей";
-	}
-	
+
 	return "error";
 }
