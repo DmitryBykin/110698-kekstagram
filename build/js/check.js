b5b3f8d68505a6d263c655a2d1731c48e5acd54c
Dmitry Bykin <dmitrybykin@gmail.com>
2016-08-04 17:53:47 +0400

    Добавлены else

--- build/js/check.js
+++ build/js/check.js
@@ -5,25 +5,26 @@ var getMessage = function(a, b){
 			return "Переданное GIF-изображение анимировано и содержит [" + b + "] кадров";
 		}
 		return "Переданное GIF-изображение не анимировано";
-	}
-	if (typeof(a) === "number") {
-		return "Переданное SVG-изображение содержит [" + a + "] объектов и [" + b * 4 + "] атрибутов";
-	}
-	if (a) {
-		if(b) {
-			var minLength = Math.min(a.length, b.length);
-			var artifactsSquare = 0;
-			var i;
-			for(i = 0; i < minLength; i++) {
-				artifactsSquare += a[i]*b[i];
-			}
-			return "Общая площадь артефактов сжатия: [" + artifactsSquare + "] пикселей";
+	}	else {
+			if (typeof(a) === "number") {
+				return "Переданное SVG-изображение содержит [" + a + "] объектов и [" + b * 4 + "] атрибутов";
+			}	else {
+					if (a) {
+						if(b) {
+							var minLength = Math.min(a.length, b.length);
+							var artifactsSquare = 0;
+							var i;
+							for(i = 0; i < minLength; i++) {
+								artifactsSquare += a[i]*b[i];
+							}
+							return "Общая площадь артефактов сжатия: [" + artifactsSquare + "] пикселей";
+						}
+						var amountOfRedPoints = a.reduce(function(sum, current){
+							return sum + current;
+						}, 0);
+						return "Количество красных точек во всех строчках изображения: [" + amountOfRedPoints + "]";
+					}
+				}
 		}
-		var amountOfRedPoints = a.reduce(function(sum, current){
-			return sum + current;
-		}, 0);
-		return "Количество красных точек во всех строчках изображения: [" + amountOfRedPoints + "]";
-	}
-
 	return "error";
-}
+};
