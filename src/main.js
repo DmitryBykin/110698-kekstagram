d3cc0ea72fe31126f09e50a21863b90f0dfb0a8c
Dmitry Bykin <bykin2002@mail.ru>
2016-08-22 00:01:24 +0400

    Задание Больше модулей

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
-require('./resizer.js');
-require('./upload.js');
-require('./pictures.js');
+define(['./pictures', './review', './load', './resizer', './upload', ], function(picturesContainer, getPictureElement, load) {
+  load('http://localhost:1506/api/pictures', 'getPictures');
+  window.getPictures = function(data) {
+    data.forEach(function(picture) {
+      getPictureElement(picture, picturesContainer);
+    });
+  };
+});
