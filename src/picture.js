6b7d29caec1fd5a952b9075862a3647fba781bb8
Dmitry Bykin <dmitrybykin@gmail.com>
2016-08-29 10:52:36 +0400

    Исправлена функция-обработчик. Переменная elements преобразуется в массив перед обработкой.

--- src/picture.js
+++ src/picture.js
@@ -4,6 +4,7 @@
   define(function() {
     return function(gallery, picturesContainer) {
       var elements = picturesContainer.querySelectorAll('.picture');
+      elements = Array.prototype.slice.call(elements); // делаем массив без этого не работало в Firefox под Linux
       elements.forEach(function(element, index) {
         element.onclick = function(evt) {
           evt.preventDefault();
